c27b43e15d365d8927aec99ee07b7f560287a7f4
amaan19 <amaan.ahmed3@gmail.com>
2019-01-31 14:50:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const getFirstSelector = selector => document.querySelector(selector)
 
+
+const nestedTarget = () => document.querySelector("#nested .target")
+
+const increaseRankBy = n => {
+    let ranks = document.querySelectorAll(".ranked-list li")
+    for (let i = 0; i < ranks.length; i++) {
+        debugger
+        ranks[i].innerHTML = (parseInt(ranks[i].innerHTML) + n).toString()
+    }
+}
+
+const deepestChild = () => {
+    return document.querySelector("#grand-node div div div div")
+}
